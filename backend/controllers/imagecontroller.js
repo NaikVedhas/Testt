851233d725ff4f8def153605ff34d6dc8606d512
NaikVedhas <vedhasnaik121@gmail.com>
2025-02-07 18:47:51 +0530

    added model

--- backend/controllers/imagecontroller.js
+++ backend/controllers/imagecontroller.js
@@ -1,9 +1,18 @@
 
 
 
-const uploadImage = async () =>{
+const uploadImage = async (req,res) =>{
     //upload to ipfs and get th cid  store to mongodb and get that id store to blockchain
-    
+    const {image} = req.body;
+
+    if(!image){
+        return res.status(400).json({message:"Image is required"});
+    }
+
+    //upload to ipfs
+
+
+    // store the hashb in db 
 
 }
 
